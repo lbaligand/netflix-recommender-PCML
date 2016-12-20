04b020cbb8dcd4d6251c9a2b7c46f6a1ca85de4f
Louis Baligand <louis.baligand@epfl.ch>
2016-12-20 20:31:17 +0100

    commenting parameter search

--- Script/parameter_search.py
+++ Script/parameter_search.py
@@ -9,17 +9,19 @@ from matrix_factorization import ALS, matrix_factorization_SGD
 def finding_gamma(train, test, gamma_array, lambda_user, lambda_item, num_features):
     """ Compute the train and test RMSE of the SGD prediction using the given parameters for each value in the gamma_array.
 
-    :param train: train dataset
-    :param test: test dataset
-    :param gamma_array: the gamma values we will train
-    :param lambda_user: the regularization SGD parameter
-    :param lambda_item: the the regularization SGD parameter
+    :param train: train dataset of shape (num_items, num_users)
+    :param test: test dataset of shape (num_items, num_users)
+    :param gamma_array: the gamma values that we will use to train the data
+    :param lambda_user: the regularization SGD parameter for the user features matrix
+    :param lambda_item: the regularization SGD parameter for the item features matrix
     :param num_features: the number of features of our item's and user's matrices
     :return: the train RMSE and the test RMSE for each gamma
     """
+    # Initialize the errors array for train and test
     rmse_train_array = []
     rmse_test_array = []
 
+    # The non zero indices for train and test
     nz_row_te, nz_col_te = test.nonzero()
     nz_test = list(zip(nz_row_te, nz_col_te))
     nz_row_tr, nz_col_tr = train.nonzero()
@@ -27,8 +29,12 @@ def finding_gamma(train, test, gamma_array, lambda_user, lambda_item, num_featur
 
     for gamma in gamma_array:
         print("predicting for gamma {}".format(gamma))
+
+        # Apply SGD to find the prediction for the selected gamma
         predicted_user_features, predicted_item_features = matrix_factorization_SGD(train, test, gamma, lambda_user,
                                                                                     lambda_item, num_features)
+
+        # Add the RMSE in the arrays
         rmse_train_array.append(compute_error(train, predicted_user_features, predicted_item_features, nz_train))
         rmse_test_array.append(compute_error(test, predicted_user_features, predicted_item_features, nz_test))
     print(np.array(rmse_test_array))
@@ -37,13 +43,29 @@ def finding_gamma(train, test, gamma_array, lambda_user, lambda_item, num_featur
 
 def find_min_num_ratings(min_num_ratings_array, ratings, num_items_per_user, num_users_per_item, p_test, lambda_item,
                          lambda_user, num_features):
+    """ Compute the train and test RMSE of ALS for a set of minimum number of ratings for users and items
 
+    :param min_num_ratings_array: array of minimum number of ratings
+    :param ratings: sparse matrix containing the data, i.e. the ratings
+    :param num_items_per_user: number of items per user
+    :param num_users_per_item: number of user per item
+    :param p_test: probability that a ratings is in the test set
+    :param lambda_item: the regularization ALS parameter for the item features matrix
+    :param lambda_user: the regularization ALS parameter for the user features matrix
+    :param num_features: the number of features of our item's and user's matrices
+    :return: the full reconstructed item and user features matrices and the train RMSE and the test RMSE for each
+             minimum number of ratings
+    """
+    # Initialization of the arrays to store the rmse and the fully reconstructed features matrices
     full_user_features_array = []
     full_item_features_array = []
     rmse_test_full_array = []
     rmse_train_full_array = []
+
     for min_num_ratings in min_num_ratings_array:
         print("Minimum number of ratings : {}".format(min_num_ratings))
+
+        # Split the data ratings with probability p_test
         valid_ratings, train, test, valid_users, valid_items, train_full, test_full = split_data(
             ratings, num_items_per_user, num_users_per_item, min_num_ratings, p_test)
 
@@ -116,7 +138,7 @@ def finding_lambdas(train, test, lambda_user_array, lambda_item_array, num_featu
     return np.array(rmse_train_array), np.array(rmse_test_array)
 
 
-def finding_weighted_average(test,predictions_a, predictions_b):
+def finding_weighted_average(test, predictions_a, predictions_b):
     """
 
     :param predictions_a:
