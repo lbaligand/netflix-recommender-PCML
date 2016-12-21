dad412f17e0af7523d4b40735ecddeb09b4620c9
Louis Baligand <louis.baligand@epfl.ch>
2016-12-21 01:28:55 +0100

    comment done

--- Script/parameter_search.py
+++ Script/parameter_search.py
@@ -65,18 +65,22 @@ def find_min_num_ratings(min_num_ratings_array, ratings, num_items_per_user, num
     for min_num_ratings in min_num_ratings_array:
         print("Minimum number of ratings : {}".format(min_num_ratings))
 
-        # Split the data ratings with probability p_test
+        # Split the data ratings with probability p_test and delete the ratings with less than min_num_ratings
         valid_ratings, train, test, valid_users, valid_items, train_full, test_full = split_data(
             ratings, num_items_per_user, num_users_per_item, min_num_ratings, p_test)
 
+        # Call ALS to get the predicted features matrices to fill
         predicted_user_features, predicted_item_features, _, _ = ALS(train, test, lambda_user, lambda_item,
                                                                      num_features)
 
+        # Reconstruct the full features matrices with the selected minimum number of ratings
         full_user_features, full_item_features = construct_full_features(predicted_user_features,
                                                                          predicted_item_features,
                                                                          valid_users, valid_items, min_num_ratings,
                                                                          train_full,
                                                                          lambda_user, lambda_item)
+
+        # Add the features matrices in the array to return
         full_user_features_array.append(full_user_features)
         full_item_features_array.append(full_item_features)
 
@@ -85,6 +89,7 @@ def find_min_num_ratings(min_num_ratings_array, ratings, num_items_per_user, num
         nz_row_tr, nz_col_tr = train_full.nonzero()
         nz_full_train = list(zip(nz_row_tr, nz_col_tr))
 
+        # Compute the RMSE for the test and trian set and add it in the array
         rmse_train_full = compute_error(train_full, full_user_features, full_item_features, nz_full_train)
         rmse_test_full = compute_error(test_full, full_user_features, full_item_features, nz_full_test)
         rmse_train_full_array.append(rmse_train_full)
@@ -93,19 +98,28 @@ def find_min_num_ratings(min_num_ratings_array, ratings, num_items_per_user, num
 
 
 def finding_num_features(train, test, num_features_array):
+    """ Compute the train and test RMSE of ALS for a set of different number of features k
+
+    :param train: train dataset of shape (num_items, num_users)
+    :param test: test dataset of shape (num_items, num_users)
+    :param num_features_array: the set of different number of features
+    :return: the train and the test RMSE and the constructed features matrices from the ALS matrix factorization for
+             each number of features
+    """
+    # Initialization
     rmse_train_array = []
     rmse_test_array = []
     item_features_array = []
     user_features_array = []
-    nz_row_te, nz_col_te = test.nonzero()
-    nz_test = list(zip(nz_row_te, nz_col_te))
-    nz_row_tr, nz_col_tr = train.nonzero()
-    nz_train = list(zip(nz_row_tr, nz_col_tr))
 
     for num_features in num_features_array:
         print("predicting for {} of features".format(num_features))
+
+        # Call the ALS algorithm to create our matrix factorization for the prediction
         predicted_user_features, predicted_item_features, rmse_train_table, rmse_test = ALS(train, test, 0.09, 0.09,
                                                                                             num_features)
+
+        # Add the features matrices and the RMSEs in the corresponding arrays
         item_features_array.append(predicted_item_features)
         user_features_array.append(predicted_user_features)
         rmse_train_array.append(rmse_train_table[len(rmse_train_table) - 1])
@@ -114,14 +128,26 @@ def finding_num_features(train, test, num_features_array):
 
 
 def finding_lambdas(train, test, lambda_user_array, lambda_item_array, num_features):
+    """ Compute the train and test RMSE of the ALS prediction using different combinations lambda user and lambda item.
+
+    :param train: train dataset of shape (num_items, num_users)
+    :param test: test dataset of shape (num_items, num_users)
+    :param lambda_user_array: the array of regularization ALS parameters for the user features matrix
+    :param lambda_item_array: the array of regularization ALS parameters for the item features matrix
+    :param num_features: the number of features of our item's and user's matrices
+    :return: the train RMSE and the test RMSE for each combination of lambdas
+    """
+    # Initialization
     rmse_train_array = []
     rmse_test_array = []
 
+    # Find the non zero indices of the test and train sets
     nz_row_te, nz_col_te = test.nonzero()
     nz_test = list(zip(nz_row_te, nz_col_te))
     nz_row_tr, nz_col_tr = train.nonzero()
     nz_train = list(zip(nz_row_tr, nz_col_tr))
 
+    # For each combinations of lambdas, calculate the predicted features matrices using ALS and store their RMSE
     for lambda_user in lambda_user_array:
         rmse_train_array_user = []
         rmse_test_array_user = []
@@ -134,26 +160,31 @@ def finding_lambdas(train, test, lambda_user_array, lambda_item_array, num_featu
             rmse_test_array_user.append(compute_error(test, predicted_user_features, predicted_item_features, nz_test))
         rmse_train_array.append(rmse_train_array_user)
         rmse_test_array.append(rmse_test_array_user)
-    # print(np.array(rmse_test_array))
     return np.array(rmse_train_array), np.array(rmse_test_array)
 
 
 def finding_weighted_average(test, predictions_a, predictions_b):
-    """
+    """ Compute the weighted average of two predictions A and B and find the weight that minimizes the RMSE
 
-    :param predictions_a:
-    :param predictions_b:
-    :return:
+    :param test: test dataset of shape (num_items, num_users)
+    :param predictions_a: matrix prediction A
+    :param predictions_b: matrix prediction B
+    :return: the weight for a that minimizes the RMSE
     """
+    # Initialization
     a = np.linspace(0, 1.0, num=101)
     rmse_min = 10
     a_min = 0
 
-    # for j in range(lambda_item_list.shape[0]):
     for i, value in enumerate(a):
+        # Compute the weighted average of the two predictions
         prediction_from_two = np.multiply(predictions_a, value) + np.multiply(predictions_b, 1 - value)
         x, y = test.nonzero()
+
+        # Calculate the RMSE of the prediction
         rmse = np.sqrt(calculate_mse(test[x, y], prediction_from_two[x, y]).sum() / (test.nnz))
+
+        # Check whether it is the optimal weight
         if rmse_min > rmse:
             rmse_min = rmse
             a_min = value
--- Script/plots.py
+++ Script/plots.py
@@ -6,16 +6,11 @@ import matplotlib.pyplot as plt
 
 
 def plot_raw_data(ratings):
-    """plot the statistics result on raw rating data.
-    
-    Args:
-        ratings: The ratings matrix of shape (num_item, num_user)
-    
-    Return:
-        num_items_per_user: Array of size num_user containing number of items
-                            per user.
-        num_users_per_item: Array of size num_item containing number of users
-                            per item.             
+    """ Plot the statistics result on raw rating data.
+
+    :param ratings: ratings matrix of shape (num_items, num_users)
+    :return: num_items_per_user of size num_user containing number of items per user and num_users_per_item of size
+             num_item containing number of users per item
     """
     # do statistics.
     num_items_per_user = np.array((ratings != 0).sum(axis=0)).flatten()
@@ -41,12 +36,15 @@ def plot_raw_data(ratings):
     plt.tight_layout()
     plt.savefig("stat_ratings")
     plt.show()
-    # plt.close()
     return num_items_per_user, num_users_per_item
 
 
 def plot_train_test_data(train, test):
-    """visualize the train and test data."""
+    """ Visualize the train and test data.
+
+    :param train: train dataset
+    :param test: test dataset
+    """
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 2, 1)
     ax1.spy(train, precision=0.01, markersize=0.5)
@@ -62,7 +60,14 @@ def plot_train_test_data(train, test):
     plt.savefig("train_test")
     plt.show()
 
+
 def plot_min_ratings(parameter_array, rmse_train_array, rmse_test_array):
+    """ Plot the RMSE test and train with respect to the minimum number of ratings
+
+    :param parameter_array: minimum number of ratings values
+    :param rmse_train_array: train RMSE for each minimum number of ratings
+    :param rmse_test_array: test RMSE for each minimum number of ratings
+    """
     fig = plt.figure()
 
     ax1 = fig.add_subplot(1, 2, 1)
@@ -83,14 +88,22 @@ def plot_min_ratings(parameter_array, rmse_train_array, rmse_test_array):
 
 
 def plot_number_features(parameter_array, rmse_train_array, rmse_test_array):
+    """ Plot the RMSE test and train with respect to number of features
+
+    :param parameter_array: number of features values
+    :param rmse_train_array: train RMSE for each number of features
+    :param rmse_test_array: test RMSE for each number of features
+    """
     fig = plt.figure()
 
+    # Plot for RMSE train
     ax1 = fig.add_subplot(1, 2, 1)
     ax1.plot(parameter_array, rmse_train_array, color='red')
     ax1.set_xlabel("number of features")
     ax1.set_ylabel("RMSE train")
     ax1.grid()
 
+    # Plot for RMSE test
     ax2 = fig.add_subplot(1, 2, 2)
     ax2.plot(parameter_array, rmse_test_array, color='red')
     ax2.set_xlabel("number of features")
@@ -103,6 +116,13 @@ def plot_number_features(parameter_array, rmse_train_array, rmse_test_array):
 
 
 def plot_every_lambda(results_test_lambda, lambda_table_user, lambda_table_item):
+    """ Plot the test RMSE with respect to the lambdas of item for every lambda user
+
+    :param results_test_lambda: test RMSEs for every combinations of lambdas
+    :param lambda_table_user: regularizer parameter values in ALS for the user features matrix
+    :param lambda_table_item: regularizer parameter values in ALS for the item features matrix
+    """
+    # Initialize parameters
     width = 12
     height = 8
     fig = plt.figure(figsize=(width, height))
@@ -110,12 +130,11 @@ def plot_every_lambda(results_test_lambda, lambda_table_user, lambda_table_item)
     x = np.array(["b", "g", "r", "c", "m", "y", "k", "pink", "orange", "indigo", "brown", "gray", "lightblue"])
     lambda_new_axis = np.linspace(lambda_table_item.min(), lambda_table_item.max(), 100)
 
+    # For a fixed lambda user, plot the test RMSE with respect to the lambdas of item
     for i, row in enumerate(results_test_lambda):
         s = np.poly1d(np.polyfit(lambda_table_item, row, 7))
         results_smooth = s(lambda_new_axis)
-        # results_smooth = itp.spline(lambda_table_item, row, lambda_new_axis)
         ax1.plot(lambda_new_axis, results_smooth, color=x[i], label="lambda user = {}".format(lambda_table_user[i]))
-        # ax1.plot(lambda_table_item, row, color = x[i], label="lambda user = {}".format(lambda_table_user[i]))
 
     ax1.set_xlabel("lambda item")
     ax1.set_ylabel("rmse test")
@@ -123,5 +142,3 @@ def plot_every_lambda(results_test_lambda, lambda_table_user, lambda_table_item)
     ax1.grid()
     ax1.set_title("Test sample")
     plt.savefig("estimation_lambda_complete")
-
-
